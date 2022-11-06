4e21ed325de19b83b05f9cf5080590e4aa62533f
krish sanghvi <krish.p.sanghvi@gmail.com>
2022-11-06 23:24:24 +0530

    Quicksort

---
+++ Quicksort.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct node {
+	int data;
+	struct node* link;
+};
+
+void add_nextnode(struct node** head, int new_info)
+{
+	struct node* new
+		= (struct node*)malloc(sizeof(struct node));
+	new->data = new_info;
+	new->link = (*head);
+	(*head) = new;
+}
+
+struct node* end(struct node* head)
+{
+    struct node* temp = head;
+    while (temp != NULL && temp->link != NULL) {
+        temp = temp->link;
+    }
+    return temp;
+}
+
+
+struct node* parition(struct node* start, struct node* end)
+{
+	struct node* pivot = start; //pivot is the first node
+	struct node* head = start; 
+	int temp = 0; //temp variable to swap data
+	
+	while (head != NULL && head != end) {
+		if (head->data < end->data) {
+			pivot = start;
+
+			// Swapping node values
+			temp = start->data;
+			start->data = head->data;
+			head->data = temp;
+
+			// Moving to next node
+			start = start->link;
+		}
+
+		//	// Moving to next node
+		head = head->link;
+	}
+
+	// Swapping node values
+	temp = start->data;
+	start->data = end->data;
+	end->data = temp;
+	return pivot;
+}
+
+void quick(struct node* start, struct node* end)
+{
+	if (start == end) {
+		return;
+	}
+	struct node* pivot = parition(start, end);
+
+	if (pivot != NULL && pivot->link != NULL) {
+		quick(pivot->link, end);
+	}
+
+	if (pivot != NULL && start != pivot) {
+		quick(start, pivot);
+	}
+}
+
+/* Function to print linked list */
+void print_data(struct node *head){
+    if (head == NULL){
+        printf("The list is empty");
+    }
+    struct node *pointer = NULL;
+    pointer = head;
+    while (pointer != NULL){
+        printf("%d ",pointer->data);
+        pointer = pointer->link;
+    }
+}
+
+int main()
+{
+	struct node* unsorted = NULL;
+
+	// Create linked list
+	add_nextnode(&unsorted, 45343);
+	add_nextnode(&unsorted, 86);
+	add_nextnode(&unsorted, 674);
+	add_nextnode(&unsorted, 1323);
+	printf("\nBefore Sorting ");
+	print_data(unsorted);
+
+	// Function call
+	quick(unsorted, end(unsorted));
+	printf("\nAfter Sorting ");
+	print_data(unsorted);
+	return 0;
+}
+
+
+
+
+
